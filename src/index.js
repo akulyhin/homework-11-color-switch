2764651e51f10b4cfeb2bf0015a731777b9e3437
Artem Kulyhin <akulyhin@gmail.com>
2020-10-16 13:10:32 +0300

    hw-11: completed

--- src/index.js
+++ src/index.js
@@ -1,1 +1,44 @@
 import './styles.css';
+
+const refs = {
+    body: document.querySelector('body'),
+    start: document.querySelector('[data-action="start"]'),
+    stop: document.querySelector('[data-action="stop"]')
+}
+
+const colors = [
+    '#FFFFFF',
+    '#2196F3',
+    '#4CAF50',
+    '#FF9800',
+    '#009688',
+    '#795548',
+  ];
+
+let changeThemeColor = false;
+
+const changeTheme = () => {
+    const randomIntegerFromInterval = (min, max) => {
+        return Math.floor(Math.random() * (max - min + 1) + min);
+      };
+      refs.body.style.background = colors[randomIntegerFromInterval(0, colors.length - 1)];
+  };
+
+function startChangeColor() {
+        changeThemeColor = setInterval(changeTheme, 1000);
+};
+
+function stopChangeColor () {
+        clearInterval(changeThemeColor);
+};
+
+refs.start.addEventListener('click', () => {
+    refs.start.disabled = true;
+    startChangeColor();
+});
+
+refs.stop.addEventListener('click', () => {
+    refs.start.disabled = false;
+    stopChangeColor();
+    refs.body.style.background = 'url(images/bg-pattern.png)';
+})
